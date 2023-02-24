fffdab65ad272485a8c24d34262534034252c646
shreshth <shreshthsrivastava2@gmail.com>
2023-02-23 17:29:10 -0800

    [data] Read data.

---
+++ data_processing.py
@@ -0,0 +1,11 @@
+# %%
+import numpy as np
+import pandas as pd
+
+DATA_PATH = 'data/data.csv'
+MOVIES_PATH = 'data/movies.csv'
+
+data = pd.read_csv(DATA_PATH, sep=',')
+movies = pd.read_csv(MOVIES_PATH, sep=',')
+
+# %%
